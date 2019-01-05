00b4b29e4ecc08d2d31ca8d31c0f3ea288021309
Hutao <hutao_hn@163.com>
2019-01-06 00:30:23 +0800

    UI界面编码

---
+++ src/components/Lists.vue
@@ -0,0 +1,84 @@
+<template>
+  <ul class="todo-main">
+    <li>
+      <label>
+        <input type="checkbox">
+        <span>Vue-拼多多项目实战-课程讲解</span>
+      </label>
+      <button style="display:none">删除</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox">
+        <span>Vue-拼多多项目实战-作业布置</span>
+      </label>
+      <button style="display:none">删除</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "lists"
+};
+</script>
+
+<style scoped>
+/*列表*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+
+/*列表选项*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  background-color: orangered;
+  padding: 4px 10px;
+  border-radius: 5px;
+  color: #fff;
+  border: none;
+  float: right;
+  margin-top: 3px;
+  outline: none;
+  cursor: pointer;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <Header></Header>
+        <Lists></Lists>
+        <Footer></Footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "./../components/Header";
+import Lists from "./../components/Lists";
+import Footer from "./../components/Footer";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    Header,
+    Lists,
+    Footer
   }
-}
+};
 </script>
+
+<style scoped>
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
+
